fb55f0a4387096fc98ead166a29db7d52d812b56
danianise <danianisehoey@gmail.com>
2022-10-23 11:15:12 -0400

    Reversed order of projects, best at the beginning

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,62 +15,40 @@ function Projects() {
     >
 
         <div className='projects'>
-            
-            <h1>
-                <a href='https://danianise.github.io/triviagame/' target='_blank'>
-                    Harry Potter Trivia
-                </a>
-            </h1>
-            <h5>
-                HTML | CSS | JavaScript
-            </h5>
-            <p className='description'>
-                A self-scoring trivia game about Harry Potter and the Wizarding World.
-                {/* Questions will increase in difficulty as the game progresses.
-                This game has 7 levels of difficulty, each with 7 questions to answer.
-                The levels are both numbered and given a name based on a Harry Potter character
-                whose magical skill level corresponds to the difficulty of questions in that level.
-                If the user is answering correctly, they will progress through the levels quickly.
-                If the user is answering incorrectly, they will stay in the same level of difficulty longer.
-                At the end of the game, a new screen will appear to show the user their final score
-                and give them a rating based on how well they did. */}
-            </p>
-            <a href='https://danianise.github.io/triviagame/' target='_blank'>
-                <div className='HPImages images'>
-                    <img src='HPTriviaPlay.png' alt='Harry Potter Trivia game play screenshot'/>
-                    <img src='HPTriviaEnd.png' alt='Harry Potter Trivia end of game screenshot'/>
-                </div>
-            </a>
-            <a href='https://github.com/danianise/triviagame' target='_blank'>
-                <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
-            </a>
-            <hr className='divider'/>
 
-            <h1>
-                <a href='http://danianise-whats-for-dinner.herokuapp.com/recipes' target='_blank'>
-                    What's For Dinner?
+        <h1>
+                <a href='https://thealumninetwork.netlify.app/' target='_blank'>
+                    [theAlumniNetwork]
                 </a>
             </h1>
             <h5>
-                HTML | EJS | CSS | JavaScript | Express | Node.js | MongoDB/Mongoose
+                JavaScript | React.js | Node.js | Django | PostgreSQL | JWT Authentication 
             </h5>
             <p className='description'>
-                This project helps to alleviate the tedium of deciding what to make for dinner every day.
-                'What's For Dinner?' is a collection of dinner recipes that can be filtered by different
-                criteria, such as ingredients or (lack of) allergens.
+            Modeled after early Facebook, [theAlumniNetwork] is an app to connect alumni of a school or course
+            for continued networking and support.
+            {/* Modeled after early Facebook, users can set up a  profile with their contact information,
+            create posts and comments in different conversation topics, and view/add to a list of relevant
+            events. Users can register and login to view protected routes, and can also update or delete
+            their own posts or comments, and update their profile information as needed. */}
             </p>
-            <a href='http://danianise-whats-for-dinner.herokuapp.com/recipes' target='_blank'>
-                <div className='recipeImages images'>
-                    <img src='recipeIndex.png' alt='Index page of Whats For Dinner'/>
-                    <img src='recipeEdit.gif' alt='gif of editing a recipe'/>
+            <a href='https://thealumninetwork.netlify.app/' target='_blank'>
+                <div className='alumniNetworkImages images'>
+                    <img src='ANLandingPage.png' alt='Alumni Network landing page'/>
+                    <img src='ANProfilePage.png' alt='Alumni Network profile page'/>
                     <br />
-                    <img src='recipeSearch.gif' alt='gif of searching by ingredients'/>
-                    <img src='allergenSearch.gif' alt='gif of filtering out allergens' />
+                    <img src='ANConversationPage.png' alt='Alumni Network landing page'/>
+                    <img src='ANEventPage.png' alt='Alumni Network profile page'/>
                 </div>
             </a>
-            <a href='https://github.com/danianise/WhatsForDinner' target='_blank'>
-                <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
-            </a>
+            <div className='multiCat'>
+                <a href='https://github.com/danianise/AlumniNetworkFrontend' target='_blank'>
+                    <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
+                </a>
+                <a href='https://github.com/danianise/AlumniNetworkBackend' target='_blank'>
+                    <img className='octocat secondOctocat' src='Octocat.png' alt='GitHub Octocat'/>
+                </a>
+            </div>
             <hr className='divider'/>
 
             <h1>
@@ -105,39 +83,63 @@ function Projects() {
                 </a>
             </div>
             <hr className='divider'/>
-
+            
             <h1>
-                <a href='https://thealumninetwork.netlify.app/' target='_blank'>
-                    [theAlumniNetwork]
+                <a href='http://danianise-whats-for-dinner.herokuapp.com/recipes' target='_blank'>
+                    What's For Dinner?
                 </a>
             </h1>
             <h5>
-                JavaScript | React.js | Node.js | Django | PostgreSQL | JWT Authentication 
+                HTML | EJS | CSS | JavaScript | Express | Node.js | MongoDB/Mongoose
             </h5>
             <p className='description'>
-            An app to connect alumni of a school or course for continued networking and support.
-            {/* Modeled after early Facebook, users can set up a  profile with their contact information,
-            create posts and comments in different conversation topics, and view/add to a list of relevant
-            events. Users can register and login to view protected routes, and can also update or delete
-            their own posts or comments, and update their profile information as needed. */}
+                This project helps to alleviate the tedium of deciding what to make for dinner every day.
+                'What's For Dinner?' is a collection of dinner recipes that can be filtered by different
+                criteria, such as ingredients or (lack of) allergens.
             </p>
-            <a href='https://thealumninetwork.netlify.app/' target='_blank'>
-                <div className='alumniNetworkImages images'>
-                    <img src='ANLandingPage.png' alt='Alumni Network landing page'/>
-                    <img src='ANProfilePage.png' alt='Alumni Network profile page'/>
+            <a href='http://danianise-whats-for-dinner.herokuapp.com/recipes' target='_blank'>
+                <div className='recipeImages images'>
+                    <img src='recipeIndex.png' alt='Index page of Whats For Dinner'/>
+                    <img src='recipeEdit.gif' alt='gif of editing a recipe'/>
                     <br />
-                    <img src='ANConversationPage.png' alt='Alumni Network landing page'/>
-                    <img src='ANEventPage.png' alt='Alumni Network profile page'/>
+                    <img src='recipeSearch.gif' alt='gif of searching by ingredients'/>
+                    <img src='allergenSearch.gif' alt='gif of filtering out allergens' />
                 </div>
             </a>
-            <div className='multiCat'>
-                <a href='https://github.com/danianise/AlumniNetworkFrontend' target='_blank'>
-                    <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
+            <a href='https://github.com/danianise/WhatsForDinner' target='_blank'>
+                <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
+            </a>
+            <hr className='divider'/>
+
+            <h1>
+                <a href='https://danianise.github.io/triviagame/' target='_blank'>
+                    Harry Potter Trivia
                 </a>
-                <a href='https://github.com/danianise/AlumniNetworkBackend' target='_blank'>
-                    <img className='octocat secondOctocat' src='Octocat.png' alt='GitHub Octocat'/>
-                </a>
-            </div>
+            </h1>
+            <h5>
+                HTML | CSS | JavaScript
+            </h5>
+            <p className='description'>
+                A self-scoring trivia game about Harry Potter and the Wizarding World.
+                {/* Questions will increase in difficulty as the game progresses.
+                This game has 7 levels of difficulty, each with 7 questions to answer.
+                The levels are both numbered and given a name based on a Harry Potter character
+                whose magical skill level corresponds to the difficulty of questions in that level.
+                If the user is answering correctly, they will progress through the levels quickly.
+                If the user is answering incorrectly, they will stay in the same level of difficulty longer.
+                At the end of the game, a new screen will appear to show the user their final score
+                and give them a rating based on how well they did. */}
+            </p>
+            <a href='https://danianise.github.io/triviagame/' target='_blank'>
+                <div className='HPImages images'>
+                    <img src='HPTriviaPlay.png' alt='Harry Potter Trivia game play screenshot'/>
+                    <img src='HPTriviaEnd.png' alt='Harry Potter Trivia end of game screenshot'/>
+                </div>
+            </a>
+            <a href='https://github.com/danianise/triviagame' target='_blank'>
+                <img className='octocat' src='Octocat.png' alt='GitHub Octocat'/>
+            </a>
+            <hr className='divider'/>
 
             <h1>
                 <a href='https://oscarsbirthday.netlify.app/' target='_blank'>
